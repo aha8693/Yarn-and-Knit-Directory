14b95045703fda87a289b1e8dcecc7b339a24237
Anna <hyeanan@gmail.com>
2026-02-11 22:55:13 -0500

    minor change for project tab ui

--- src/components/ProjectTab.jsx
+++ src/components/ProjectTab.jsx
@@ -93,7 +93,7 @@ export function ProjectTab() {
               No projects added yet.
             </p>
           ) : (
-            <div className="space-y-2">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
               {projects.map((project) => (
                 <div
                   key={project.id}
